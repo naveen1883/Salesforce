9ebc2e0adccd5cb54bb9e45c87f7375d649495a9
Reaper1883 <30424334+Reaper1883@users.noreply.github.com>
2019-08-05 23:21:10 -0400

    Update and rename searchAccountHelper.js.rtf to searchAccountHelper.js

---
+++ AuraComponentsAssignment/searchAccountHelper.js
@@ -0,0 +1,50 @@
+({
+    SearchHelper : function(component,event) {
+        // show spinner message
+         component.find("Id_spinner").set("v.class" , 'slds-show');
+        var action = component.get("c.fetchAccount");
+        action.setParams({
+            'searchKeyWord': component.get("v.searchKeyword")
+        });
+        action.setCallback(this, function(response) {
+           // hide spinner when response coming from server 
+            component.find("Id_spinner").set("v.class" , 'slds-hide');
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var storeResponse = response.getReturnValue();
+                
+                // if storeResponse size is 0, display no record found message on screen.
+                if (storeResponse.length == 0) {
+                    component.set("v.Message", true);
+                } 
+                
+                else {
+                    component.set("v.Message", false);
+                }
+                
+                // set numberOfRecord attribute value with length of return value from server
+                component.set("v.TotalNumberOfRecord", storeResponse.length);
+                
+                // set searchResult list with return value from server.
+                component.set("v.searchResult", storeResponse); 
+                
+            }
+            else if (state === "INCOMPLETE") {
+                alert('Response is Incompleted');
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        alert("Error message: " + 
+                                    errors[0].message);
+                    }
+                } 
+                else {
+                    alert("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+})
