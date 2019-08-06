8d83a96f9a91b46faad10eabffdb971799af0a25
Reaper1883 <30424334+Reaper1883@users.noreply.github.com>
2019-08-05 23:15:57 -0400

    Update and rename AddNewAccountController.js.rtf to AddNewAccountController.js

---
+++ AuraComponentsAssignment/AddNewAccountController.js
@@ -0,0 +1,43 @@
+({
+    doInit : function(component,event,helper){
+  		//Do nothing on load
+ 	},
+ 	closedModal: function(component,event,helper){
+       helper.closedModal();         
+    },
+    saveAccount : function(component,event,helper){
+       //getting the candidate information
+        var accountRecord = component.get("v.accObj");
+        //Calling the Apex Function
+        var action = component.get("c.createAccount");
+        
+        //Setting the Apex Parameter
+        action.setParams({
+            acc : accountRecord
+        });
+        
+        //Setting the Callback
+        action.setCallback(this,function(a){
+            //get the response state
+            var state = a.getState();
+            
+            //check if result is successfull
+            if(state == "SUCCESS"){
+                //Reset Form
+                var newAccount = {'sobjectType': 'Account',
+                                    'Name': '',
+                                    'Phone': ''
+                                   };
+                
+                //resetting the Values in the form
+                component.set("v.accObj",newAccount);
+            } 
+           else if(state == "ERROR"){
+                alert('Error in calling server side action');
+            }
+        });
+ 		//adds the server-side action to the queue        
+        $A.enqueueAction(action);
+        helper.closedModal(); //calls helper controller method to close the popup and refresh the account list. 
+    }
+})
