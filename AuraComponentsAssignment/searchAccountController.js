9a3ac3681d820ef4b468314e77abbf78b9308b33
Reaper1883 <30424334+Reaper1883@users.noreply.github.com>
2019-08-09 22:02:35 -0400

    Update searchAccountController.js

--- AuraComponentsAssignment/searchAccountController.js
+++ AuraComponentsAssignment/searchAccountController.js
@@ -8,12 +8,22 @@
             searchField.focus();
         }
         else {
-          // else call helper function 
+          	// else call helper function 
             helper.SearchHelper(component, event);
         }
     },
+   	doInit : function(component,event,helper){
+  		helper.getAccountRecord(component); // Calling Helper method on load
+ 	},
+    showAccountModal : function(component,event,helper){
+        component.set("v.displayNewAccount",true); // calls to show popup
+    },
+    newClosedModal : function(component,event,helper){
+        component.set("v.displayNewAccount",false);
+        helper.getAccountRecord(component); // Calling Helper method to display record after record save from popup
+    },
     handleClick : function(component, event, helper) {
-        var Idx = event.target.id;
+        var Idx = event.getSource().get("v.value");
         var baseUrl = "https://resourceful-wolf-b2vzfu-dev-ed.lightning.force.com/lightning/r/Account/";
         var endUrl = "/view";
         window.open(baseUrl+Idx+endUrl);
