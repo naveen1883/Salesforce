0823b4621a3d5977c1a6a70e6703b0f9868fcf8b
Reaper1883 <30424334+Reaper1883@users.noreply.github.com>
2019-08-20 23:51:12 -0400

    Update ScheduleMeetingHelper.js

--- IntegrationsAssignment/ScheduleMeetingHelper.js
+++ IntegrationsAssignment/ScheduleMeetingHelper.js
@@ -1,83 +1,60 @@
 ({
-	doInit : function(component, event, helper) {
-		var action = component.get("c.getContact");
+	searchHelper : function(component,searchKeyword) {
+        var encodedKeyword = searchKeyword.replace(/ /g,"%20");
+        var action = component.get("c.getAutoCallout");
         action.setParams({
-            "contactId" : component.get("v.recordId")
+            'url' : '/autocomplete?text=' + encodedKeyword + "&latitude=35.7689&longitude=118.5473" 
         });
         action.setCallback(this,function(response){
             var state = response.getState();
             if(state === "SUCCESS"){
-                var currentContact = response.getReturnValue();
-                component.set("v.currentContact",currentContact);
-                component.set("v.mapMarkers",[{
-                    location : {
-                        Street : currentContact.MailingAddress.street,
-                        City : currentContact.MailingAddress.city,
-                        PostalCode : currentContact.MailingAddress.postalCode,
-                        State : currentContact.MailingAddress.state,
-                        Country : currentContact.MailingAddress.country
-                    },
-                    
-                    title : "Your Location"
-                }]);
-                component.set("v.center", component.get("v.mapMarkers")[0]);
-                component.set("v.markersTitle","Businesses");
-                component.set("v.showFooter",true);  
+                component.set("v.listOfAutoComplete",response.getReturnValue());
             } else {
-                console.log("ScheduleMeetingController.doInit() failed with state: " + state);
+                console.log("ScheduleMeetingHelper.searchHelper() failed with state: " + state);
             }
         });
-        $A.enqueueAction(action); 
+        $A.enqueueAction(action);
 	},
     
-    onblur : function(component, event, helper){
-        var close = component.find("searchResult");
-        $A.util.addClass(close,"slds-is-close");
-        $A.util.removeClass(close,"slds-is-open");
-    },
-    
-    onfocus : function(component, event, helper){
-        var searchKeyword = component.get("v.searchKeyword");
-        if(searchKeyword.length!=0){
-            var open = component.find("searchResult");
-            $A.util.addClass(open,"slds-is-open");
-        	$A.util.removeClass(open,"slds-is-close");
-            helper.searchHelper(component,searchKeyword);
-        }
-    },
-    
-    handleSearchEvent : function(component, event, helper){
-        var selectedBusiness = event.getParam("selectedBusiness");
-        component.set("v.searchKeyword",selectedBusiness);
-        component.set("v.selectedBusiness",selectedBusiness);
+    getBusiness : function(component,searchKeyword){
+        var encodedKeyword = searchKeyword.replace(/ /g,"%20");
+        var currentContact = component.get("v.currentContact");
+        var location = currentContact.MailingAddress.city + "," + currentContact.MailingAddress.state;
+        var encodedLocation = location.replace(/ /g,"%20");
         
-        var action = component.get("c.onblur");
+        var action = component.get("c.getSearchCallout");
+        action.setParams({
+            'url' : '/businesses/search?term=' + encodedKeyword + '&location=' + encodedLocation
+        });
+        
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                var searchResults = response.getReturnValue();
+                var mapMarkers = [];
+                for(var x = 0; x < searchResults.length; x++){
+                    mapMarkers.push({
+                        location : {
+                            Street : searchResults[x].location.address1, //first line of address returned
+                            City : searchResults[x].location.city,
+                            PostalCode : searchResults[x].location.zip_code,
+                            State : searchResults[x].location.state,
+                            Country : searchResults[x].location.country
+                        },
+                        
+                        title : searchResults[x].name,
+                        description : searchResults[x].name + ' has a rating of ' + searchResults[x].rating
+                        + ' and can be contacted at: ' + searchResults[x].phone + '.'
+                    });
+                    
+                    if(x==4) break;
+                }
+                component.set("v.mapMarkers",mapMarkers);
+                component.set("v.center",mapMarkers[0]);
+            } else {
+                console.log("ScheduleMeetingHelper.getBusinesses() failed with state: " + state);
+            }
+        });
         $A.enqueueAction(action);
-    },
-    
-    keyPressController : function(component, event, helper){
-        var searchKeyword = component.get("v.searchKeyword");
-        
-        if(searchKeyword.length > 2){
-            var open = component.find("searchResult");
-            $A.util.addClass(open,"slds-is-open");
-            $A.util.removeClass(open,"slds-is-close");
-            
-            helper.searchHelper(component,searchKeyword);
-        } else {
-            component.set("v.listOfAutoComplete",[]);
-            var action = component.get("c.onblur");
-            $A.enqueueAction(action);
-        }
-    },
-    
-    keySearch : function(component, event, helper){
-        if(event.which == 13){
-            var searchKeyword = component.get("v.searchKeyword");
-            helper.getBusiness(component,searchKeyword);
-            
-            var action = component.get("c.onblur");
-            $A.enqueueAction(action);
-        }
     }
 })
