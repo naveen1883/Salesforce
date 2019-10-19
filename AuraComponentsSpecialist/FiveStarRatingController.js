0979a715204ed87cf672d6fc82457620e8247d9e
Naveen Nagarani Saravanan <sns100@gmail.com>
2019-10-18 23:44:17 -0500

    Create FiveStarRatingController.js

---
+++ AuraComponentsSpecialist/FiveStarRatingController.js
@@ -0,0 +1,27 @@
+({
+  afterScriptsLoaded: function(component, event, helper) {
+    debugger;
+    var domEl = component.find("ratingarea").getElement();
+
+    var currentRating = component.get("v.value");
+    var readOnly = component.get("v.readonly");
+    var maxRating = 5;
+    var callback = function(rating) {
+      component.set("v.value", rating);
+    };
+    component.ratingObj = rating(
+      domEl,
+      currentRating,
+      maxRating,
+      callback,
+      readOnly
+    );
+  },
+
+  onValueChange: function(component, event, helper) {
+    if (component.ratingObj) {
+      var value = component.get("v.value");
+      component.ratingObj.setRating(value, false);
+    }
+  }
+});
